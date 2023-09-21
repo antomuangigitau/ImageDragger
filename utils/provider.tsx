fd94f1133b57954073419f925993f98e754f7505
antomuangigitau <muangigitau@gmail.com>
2023-09-22 00:26:14 +0300

    removed conflicting packages

--- utils/provider.tsx
+++ utils/provider.tsx
@@ -1,20 +1,12 @@
-"use client";
+'use client';
 
-import React from "react";
-import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
-// import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { ReactQueryStreamedHydration } from "@tanstack/react-query-next-experimental";
+import React from 'react';
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
 
 function Providers({ children }: React.PropsWithChildren) {
   const [client] = React.useState(new QueryClient());
 
-  return (
-    <QueryClientProvider client={client}>
-      <ReactQueryStreamedHydration>{children}</ReactQueryStreamedHydration>
-
-      {/* <ReactQueryDevtools initialIsOpen={false} /> */}
-    </QueryClientProvider>
-  );
+  return <QueryClientProvider client={client}>{children}</QueryClientProvider>;
 }
 
 export default Providers;
